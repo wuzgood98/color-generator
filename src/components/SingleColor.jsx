325ce5f43511017f4a08815b684a07b2df84afb4
Gideon Ofori Addo <gideonaddoofori@gmail.com>
2022-08-08 09:10:34 +0000

    Initial project version

--- src/components/SingleColor.jsx
+++ src/components/SingleColor.jsx
@@ -19,7 +19,7 @@ function SingleColor({ weight, rgb, index }) {
   }, [alert])
 
   return (
-    <article onClick={handleCopy} style={{ backgroundColor: hexColor }} className="relative px-8 py-4 cursor-pointer text-base normal-case">
+    <article onClick={handleCopy} style={{ backgroundColor: hexColor }} className="relative w-full px-8 py-4 cursor-pointer text-base normal-case">
       <p className={`mb-0  ${index > 7 ? 'text-clrWhite' : 'text-clrGrey1'}`}>{weight}%</p>
       <p className={`mb-0 ${index > 7 ? 'text-clrWhite' : 'text-clrGrey1'}`}>{hexColor}</p>
       {alert && <p className="absolute top-1/2 left-12 uppercase text-sm mt-2">copied to clipboard</p>}
